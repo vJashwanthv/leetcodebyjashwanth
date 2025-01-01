a3583eae2c92f321469f2642dcf9c1b170ca72c3
vJashwanthv <157822956+vJashwanthv@users.noreply.github.com>
2025-01-01 13:41:53 +0530

    Time: 0 ms, Memory: 45.5 MB - LeetHub

--- 12611-3368-268-missing-number/12611-3368-268-missing-number.java
+++ 12611-3368-268-missing-number/12611-3368-268-missing-number.java
@@ -3,11 +3,9 @@ class Solution {
         int n = nums.length;
         int xor1 = 0;
         int xor2 = 0;
-        for(int i=1;i<=n;i++){
-            xor1 ^= i;
-        }
-        for(int i : nums){
-            xor2 ^= i;
+        for(int i = 0;i < n;i++){
+            xor2 ^= nums[i];
+            xor1 ^= (i+1);
         }
         return xor1^xor2;
     }
